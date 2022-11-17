3a23117264a2c59e8730e9477874c25f6b9c62e2
Ian Ikenga <115921555+ikengadevgroup@users.noreply.github.com>
2022-11-17 14:58:44 -0600

    Updated RSS feeds

--- app.js
+++ app.js
@@ -3,10 +3,12 @@
 //Initializing Entry point and testing API
 var IFTTT_SERVICE_KEY = "-MRtrOGdwn39dcuvKTLnaIA_eaMepmDU1Bruynoy0cjx9IaVrkQ6WYRDYfKjl_eu"
 const express = require('express')
-//const res = require('express/lib/response')
 const app = express()
 
-
+const parser2 = require("fast-xml-parser");
+const got = require("fix-esm").require("got");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
 
 app.get('/api/ping', (req,res) => {
     var response = { answer: "pong" }
@@ -17,53 +19,88 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
 
-const myFeedUrl = 'http://feeds.washingtonpost.com/rss/politics?itid=lk_inline_manual_2'
+//const myFeedUrl = 'http://feeds.washingtonpost.com/rss/politics?itid=lk_inline_manual_2'
+const myFeedUrl = 'https://rss.app/feeds/_96ajWY9y0j6W2zRl.xml'
+const all ="_96ajWY9y0j6W2zRl"
+const national ="tdsav0GhLPO5siKU"
+const entertainment ="wNkhQuGEe0XypVUG"
+const business ="CVrbOhIFQ796bQMM"
+const sports ="xConq0IzudRi86i0"
+const world ="iZLI9pTD2sCeOExW"
+const lifestyle ="LRaKK8EByTTvg4PL"
+const technology ="YcUiV5eFSKfuWks9"
+const opinions ="zlzIyItLwhfkduxU"
+const politics ="kZ2HQoc2bRrPqnrm"
+
+const feedpt1 = "https://rss.app/feeds/" 
+var feedpt2 =""
 
 
 
 
-////////////////////////////////// Fast RSS Parser //////////////////////////////////////////////////////////
 
-var parser3 = require('rssparser2');
-var options = {};
-//rss feeds
-/*parser3.parseURL('http://laymansite.com/feed', options, function(err, out){
-	console.log(out);
-});
-*/
 
-////////////////////////////// End Fast Parser /////////////////////////////////////////////////////////////
 
-//////////////////////////////////////// Original RSS Parser /////////////////////////////////////////////////
 
 //Configuring RSS-Parser Package
 let Parser = require('rss-parser')
-
-//let parser = new Parser();
 
 let parser = new Parser({
     headers: { 'User-Agent': 'Chrome' }
 });
 
+function generateUnique8DigitString() {
+    let result = '';
+    for (let i = 0; i < 8; i++) {
+        result += Math.floor(Math.random() * 10);
+    }
+    return result;
+}
 
+function createAt() {
+    var date = new Date();
+    return date.toISOString();
+  }
 
-//This function is supposed ot read the RSS Feed
 async function fetchRssFeed(feedUrl) {
     let feed = await parser.parseURL(feedUrl);
-    /*
+    let data = feed.items.slice(0,3).map(item => ({
+        meta: {
+            id: generateUnique8DigitString(),
+            timestamp: Math.floor(Date.now() / 1000)
+        },
+                created_at: createAt(),
+                title: item.title,
+                link: item.link,
+                date: item.pubDate
+    }));
+  
+    let response2 = {
+        data       
+    }
+    return response2
+}  
 
-    let feed = await parser3.parseURL(feedUrl, options, function(err, out){
-        console.log(out);
-    });
-    */
-	return feed.items.map(item => {
-        return {
-            title: item.title,
-            link: item.link,
-            date: item.pubDate
-        }
-    });
-}
+
+async function fetchRssFeedOnly1(feedUrl) {
+    let feed = await parser.parseURL(feedUrl);
+    let data = feed.items.slice(0,1).map(item => ({
+        meta: {
+            id: generateUnique8DigitString(),
+            timestamp: Math.floor(Date.now() / 1000)
+        },
+                created_at: createAt(),
+                title: item.title,
+                link: item.link,
+                date: item.pubDate
+    }));
+  
+    let response2 = {
+        data       
+    }
+    return response2
+}  
+
 
 /////////////////////////////////// End Original Parser //////////////////////////////////////////////////////
 
@@ -109,7 +146,6 @@ app.get('/ifttt/v1/status', function(req, res) {
   });
   
 
-
 // Basic Test Routes
 
 app.get('/api/news', async (req, res) => {
@@ -147,16 +183,37 @@ app.get('/api/news/:searchString', async (req, res) => {
 //My tiertiary fucntion that checks a string and then searches to the corresponding section and then returns that
 app.get('/api/news/cat/:searchCategory', async (req, res) => {
     const searchCategory = req.params.searchCategory.toLowerCase()
-    var myFeedUrl2 = ""
+    //Which category did the user pick?
+    if(searchCategory == "poltiics")
+        feedpt2 = politics
+    else if(searchCategory == "business")
+        feedpt2 = business
+    else if(searchCategory == "national")
+        feedpt2 = national
+    else if(searchCategory == "entertainment")
+        feedpt2 = entertainment
+    else if(searchCategory == "sports")
+        feedpt2 = sports
+    else if(searchCategory == "world")
+        feedpt2 = world
+    else if(searchCategory == "lifestyle")
+        feedpt2 = lifestyle
+    else if(searchCategory == "business")
+        feedpt2 = business
+    else if(searchCategory == "technology")
+        feedpt2 = technology
+    else if(searchCategory == "opinions")
+        feedpt2 = opinions
+    else 
+        feedpt2 = all
+    
+    //Building the correct feed
+    var myFeedUrl2 = feedpt1 + feedpt2 + ".xml"
+    
+    //Check the if the Service Key is valid
     var serviceKey = req.header("IFTTT-Service-Key");
     if (serviceKey == IFTTT_SERVICE_KEY) {
-        if(searchCategory == "technology"){
-            myFeedUrl2 = "http://feeds.washingtonpost.com/rss/business/" + searchCategory    
-        }
-        else{
-            myFeedUrl2 = "http://feeds.washingtonpost.com/rss/" + searchCategory
-        }   
-        await fetchRssFeed(myFeedUrl2)
+       await fetchRssFeed(myFeedUrl2)
             .then(data => {
             res.status(200).json(data)
             })
@@ -170,7 +227,9 @@ app.get('/api/news/cat/:searchCategory', async (req, res) => {
     else {
         res.status(401).json({
             status: 'error',
-            message: 'Invalid Service key'
+            message: 'Invalid Service key',
+            feed_url: myFeedUrl2,
+            category: searchCategory
         })  
     }
 })
@@ -206,7 +265,7 @@ app.post('/ifttt/v1/triggers/new_article_matching_search/', async (req, res) => 
     var serviceKey = req.header("IFTTT-Service-Key");
     var searchString = req.header("term");
     
-    var Tfs = req.header('triggerFeilds');
+    
     if (serviceKey == IFTTT_SERVICE_KEY) { 
             if(req.header('triggerFeilds') == ""){
             res.status(400).json({
@@ -229,7 +288,7 @@ app.post('/ifttt/v1/triggers/new_article_matching_search/', async (req, res) => 
         .catch(err => {
             res.status(500).json({
                 status: "error",
-                message: 'An error occured when fetching your data - Tfs' + Tfs
+                message: 'An error occured when fetching your data - DUDE!'
                 })
             })
     }
@@ -247,7 +306,7 @@ app.post('/ifttt/v1/triggers/new_article_matching_search/', async (req, res) => 
                   "internal_message": "Ifttt::Protocol::AuthorizationError: Invalid IFTTT-Channel-Key"
                 }
               ],
-           notes: 'Invalid Service key ' + IFTTT_SERVICE_KEY + " == " + serviceKey + " // " + Tfs
+           notes: 'Invalid Service key ' + IFTTT_SERVICE_KEY + " == " + serviceKey
         })  
     }
 
@@ -256,27 +315,98 @@ app.post('/ifttt/v1/triggers/new_article_matching_search/', async (req, res) => 
 
 
 app.post('/ifttt/v1/triggers/news_article_from_section/', async (req, res) => {
-    var searchCategory = req.header("section");
-    var myFeedUrl2 = ""
+    
+    if(!req.hasOwnProperty('section'))
+        var searchCategory = req.body.triggerFields.section
+    
+    //Which category did the user pick?
+    if(searchCategory == "politics")
+        feedpt2 = politics
+    else if(searchCategory == "business")
+        feedpt2 = business
+    else if(searchCategory == "national")
+        feedpt2 = national
+    else if(searchCategory == "entertainment")
+        feedpt2 = entertainment
+    else if(searchCategory == "sports")
+        feedpt2 = sports
+    else if(searchCategory == "world")
+        feedpt2 = world
+    else if(searchCategory == "lifestyle")
+        feedpt2 = lifestyle
+    else if(searchCategory == "business")
+        feedpt2 = business
+    else if(searchCategory == "technology")
+        feedpt2 = technology
+    else if(searchCategory == "opinions")
+        feedpt2 = opinions
+    else 
+        feedpt2 = all
+    //Building the correct feed
+    var myFeedUrl2 = feedpt1 + feedpt2 + ".xml"
+    
+    //Check the if the Service Key is valid
     var serviceKey = req.header("IFTTT-Service-Key");
     if (serviceKey === IFTTT_SERVICE_KEY) {
-               
-        if(searchCategory == "technology"){
-            myFeedUrl2 = "http://feeds.washingtonpost.com/rss/business/" + searchCategory    
+        var n = req.body.limit
+        var dp = 0
+        if (!req.hasOwnProperty('section')) {
+            dp = 1
+            console.log('The req object is missing the triggerFields key.');
+          }
+        var tp2 = ""
+        console.log("Going in. Tf = " + req.triggerFields)
+        if (typeof searchCategory === 'undefined'){
+            console.log("Toasty bitch. Tf = " + searchCategory || dp == 1)
+            res.status(400).json({
+                status: 'error',
+                errors: [
+                    {
+                      "message": "Invalid AF",
+                                         }
+                  ],
+               notes: 'Trigger Fields are Missing',
+              
+            })  
         }
-        else{
-            myFeedUrl2 = "http://feeds.washingtonpost.com/rss/" + searchCategory + " == " + serviceKey
-        }   
-        await fetchRssFeed(myFeedUrl2)
+        
+        
+        
+        if(n ==0) {
+            res.status(200).json({
+                "data": []          
+            })
+        } 
+        else if(n==1)  
+        {
+            await fetchRssFeedOnly1(myFeedUrl2)
             .then(data => {
-            res.status(200).json(data)
+                res.status(200).json(data)
             })
             .catch(err => {
                 res.status(500).json({
-                    status: "error",
-                    message: 'An error occured when fetching your data'
+                    status: "error, search Catergory was: " + searchCategory, 
+                    message: 'An error occured when fetching your data. IFTTT-Service-Key:' + serviceKey,
+                    feed_url: myFeedUrl2
                 })
             })
+
+        }
+        else 
+        {
+        
+            await fetchRssFeed(myFeedUrl2)
+                .then(data => {
+                    res.status(200).json(data)
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        status: "error, search Catergory was: " + searchCategory, 
+                        message: 'An error occured when fetching your data. IFTTT-Service-Key:' + serviceKey,
+                        feed_url: myFeedUrl2
+                    })
+                })
+        }
     }
 
     else
@@ -293,7 +423,8 @@ app.post('/ifttt/v1/triggers/news_article_from_section/', async (req, res) => {
                   "internal_message": "Ifttt::Protocol::AuthorizationError: Invalid IFTTT-Channel-Key"
                 }
               ],
-           notes: 'Invalid Service key ' + IFTTT_SERVICE_KEY + " == " + serviceKey
+           notes: 'Invalid Service key ' + IFTTT_SERVICE_KEY + " == " + serviceKey,
+           feed_url: myFeedUrl2
         })  
 
     }
@@ -301,31 +432,54 @@ app.post('/ifttt/v1/triggers/news_article_from_section/', async (req, res) => {
 
 app.post('/ifttt/v1/triggers/news_article_from_section/:searchCategory', async (req, res) => {
     const searchCategory = req.params.searchCategory.toLowerCase()
-    var myFeedUrl2 = ""
+    
+    //Which category did the user pick?
+    if(searchCategory == "poltiics")
+        feedpt2 = politics
+    else if(searchCategory == "business")
+        feedpt2 = business
+    else if(searchCategory == "national")
+        feedpt2 = national
+    else if(searchCategory == "entertainment")
+        feedpt2 = entertainment
+    else if(searchCategory == "sports")
+        feedpt2 = sports
+    else if(searchCategory == "world")
+        feedpt2 = world
+    else if(searchCategory == "lifestyle")
+        feedpt2 = lifestyle
+    else if(searchCategory == "business")
+        feedpt2 = business
+    else if(searchCategory == "technology")
+        feedpt2 = business
+    else if(searchCategory == "opinions")
+        feedpt2 = business
+    else 
+        feedpt2 = all
+    
+    //Building the correct feed
+    var myFeedUrl2 = feedpt1 + feedpt2 + ".xml"
+    
+    //Check the if the Service Key is valid
     var serviceKey = req.header("IFTTT-Service-Key");
     if (serviceKey === IFTTT_SERVICE_KEY) {
-        if(searchCategory == "Technology"){
-            myFeedUrl2 = "http://feeds.washingtonpost.com/rss/business/" + searchCategory    
-        }
-        else{
-            myFeedUrl2 = "http://feeds.washingtonpost.com/rss/" + searchCategory
-        }   
-        await fetchRssFeed(myFeedUrl2)
+       await fetchRssFeed(myFeedUrl2)
             .then(data => {
             res.status(200).json(data)
             })
             .catch(err => {
                 res.status(500).json({
                     status: "error",
-                    message: 'An error occured when fetching your data' + IFTTT_SERVICE_KEY
+                    message: 'An error occured when fetching your data' + IFTTT_SERVICE_KEY,
+                    feed_url: myFeedUrl2
                 })
             })
     }
     else {
         res.status(401).json({
             status: 'error',
-            message: 'Invalid Service key'
+            message: 'Invalid Service key',
+            feed_url: myFeedUrl2
         })  
     }
 })
-
